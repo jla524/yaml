a2084b91cdd038dc81789ffd4d35e614ed457bd8
Jacky Lee <jla524@sfu.ca>
2022-02-08 21:13:34 -0800

    Update operations header with TODOs

--- operations.h
+++ operations.h
@@ -1,7 +1,7 @@
-// add, subtract, transpose, mul, dot, divide, reduce
 #include "matrix.h"
 
 void matrix_add(matrix *a, matrix *b, matrix *out);
 void matrix_subtract(matrix *a, matrix *b, matrix *out);
-void matrix_dot(matrix *a, matrix *b, matrix *out);
+void matrix_multiply(matrix *a, matrix *b, matrix *out);
 void matrix_transpose(matrix *a, matrix *out);
+// TODO: dot, divide, reduce
